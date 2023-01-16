494feb5bb8571fb7b00aec832acde603d49e33e6
Aaron Dreyer <aaronottaway@gmail.com>
2023-01-16 12:32:58 -0500

    Variables and Arrays
    I added a list of arrays with respective names that will be used as the prompt values for the password

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,18 @@
-// Assignment code here
+var characterLength = "";
+
+var number;
+var specialCharacter;
+var lowerCase;
+var upperCase;
+
+number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+specialCharacter = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "=", "`", "~", "[", "]", "{", "}", "/", ";", ":", ",", "<", ">", ".", "?"];
+lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -13,5 +21,4 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
