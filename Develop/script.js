d295f501e40840370701d76614b32184d9201ca0
Aaron Dreyer <aaronottaway@gmail.com>
2023-01-16 14:56:36 -0500

    Prompts
    I added the prompts needed to allow which characters you would like in the password. I also included the character length with an isNaN to accept integers. I do not know how to allow strings such as written number names.

--- Develop/script.js
+++ Develop/script.js
@@ -21,4 +21,38 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+
+}
+
+function prompts() {
+
+  characterLength = parseInt(prompt("How many characters do you want your password to be? (8 - 128 characters)"));
+
+  if (isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
+    alert("Character length must be 8 - 128 characters");
+    return false;
+  }
+
+  if (confirm("Would you like special characters in your password?")) {
+    choice = choice.concat(specialCharacter);
+  }
+
+  if (confirm("Would you like numbers in your password?")) {
+      choice = choice.concat(number);
+  }
+
+  if (confirm("Would you like upper case letters in your password?")) {
+      choice = choice.concat(upperCase);
+  }
+
+  if (confirm("Would you like lower case letters in your password?")) {
+    choice = choice.concat(lowerCase);
+  }
+
+  return true;
+
+}
+
+
 generateBtn.addEventListener("click", writePassword);
