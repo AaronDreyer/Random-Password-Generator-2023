0023f389d0cd974bda56ee992560d1516b33b6ba
Aaron Dreyer <aaronottaway@gmail.com>
2023-01-18 12:40:24 -0500

    Restructuring and README
    I restructured my javascript to make a little bit more sense logistically (even though javascript doesn't follow a logistical path very well) and also added two screenshots of the website in the README.

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,9 @@
 // array for character length
-var characterLength = "";
+var passwordLength = "";
+var promptMessage = "";
 
 // array for the choice based on prompt answers
-var choice = [];
+var passwordCriteria = [];
 // arrays for numbers available
 var number;
 // array for special characters available
@@ -22,45 +23,16 @@ upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N
 
 var generateBtn = document.querySelector("#generate");
 
-// function for writing password
-function writePassword() {
-   // for mulitple prompts that show based on password requirements
-  var completedPrompts = prompts();
-  var passwordText = document.querySelector("#password");
-
-// if-statement stating if correct prompts are met (using prompt function) then you generate password
-// if prompts are not met then error message appears
-if (completedPrompts) {
-  var userPassword = generatePassword();
-  passwordText.value = userPassword;
-} else {
-  passwordText.value = "Please try again!";
-}
-}
-
-// function for generating password
-function generatePassword() {
-  var password = "";
-   // var i = 0 is an initializer statement where we declare a variable i with value 0
-  // i++ (postfix operator) - this means that the value of the variable is incremented after it is used in the expression
-  for(var i = 0; i < characterLength; i++) {
-    var randomIndex = Math.floor(Math.random() * choice.length);
-    password = password + choice[randomIndex];
-  }
-   
-return password;
-}
-
 // function for displaying prompts and their questions
-function prompts() {
-  choice = [];
+function choices() {
+  passwordCriteria = [];
 
   // parseInt converts argument to a strong then analyzes string to be returned as an integer
-  characterLength = parseInt(prompt("How many characters do you want your password to be? (8 - 128 characters)"));
+  passwordLength = parseInt(prompt("How many characters do you want your password to be? (8 - 128 characters)"));
 
     // || is an Or Operator - it keeps evaluating expressions until it finds on that is true, and then stops
     // isNaN represents a value which is not a valid number. It can be used to check whether a number entered is a valid number or not a number
-  if (isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
     alert("Character length must be 8 - 128 characters");
     
     // The return statement ends function execution and specifies a value to be returned to the function caller.
@@ -68,25 +40,29 @@ function prompts() {
     return false;
   }
 
+  if (confirm("Please choose what characters you would like to include in your password.")) {
+    // Make prompt with only OK button?
+  } 
+
   if (confirm("Would you like special characters in your password?")) {
     // concat combines two or more items and returns them as a single item
     // concat() method is used to merge two or more arrays. This method does not change the existing arrays, but instead returns a new array.
-    choice = choice.concat(specialCharacter);
+    passwordCriteria = passwordCriteria.concat(specialCharacter);
   }
 
   if (confirm("Would you like numbers in your password?")) {
-      choice = choice.concat(number);
+    passwordCriteria = passwordCriteria.concat(number);
   }
 
   if (confirm("Would you like upper case letters in your password?")) {
-      choice = choice.concat(upperCase);
+    passwordCriteria = passwordCriteria.concat(upperCase);
   }
 
   if (confirm("Would you like lower case letters in your password?")) {
-    choice = choice.concat(lowerCase);
+    passwordCriteria = passwordCriteria.concat(lowerCase);
   }
 
-  if (choice.length == 0) {
+  if (passwordCriteria.length == 0) {
     // .length property returns the length of a string
     // if length of choice is set to 0 then no options were selected then error message pops up
     alert("Please confirm at least one prompt.")
@@ -98,5 +74,35 @@ function prompts() {
   return true;
 
 }
+
+// function for generating password
+function generatePassword() {
+  var password = "";
+   // var i = 0 is an initializer statement where we declare a variable i with value 0
+  // i++ (postfix operator) - this means that the value of the variable is incremented after it is used in the expression
+  for(var i = 0; i < passwordLength; i++) {
+    var randomIndex = Math.floor(Math.random() * passwordCriteria.length);
+    password = password + passwordCriteria[randomIndex];
+  }
+   
+return password;
+}
+
+// function for writing password
+function writePassword() {
+   // for mulitple prompts that show based on password requirements
+  var criteria = choices();
+  var passwordText = document.querySelector("#password");
+
+// if-statement stating if correct prompts are met (using prompt function) then you generate password
+// if prompts are not met then error message appears
+if (criteria) {
+  var userPassword = generatePassword();
+  passwordText.value = userPassword;
+} else {
+  passwordText.value = "Please try again!";
+}
+}
+
 
 generateBtn.addEventListener("click", writePassword);
